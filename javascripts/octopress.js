58cff30bffc43781f09202bb4e581b40d944dbd0
Robert Riemann <robert@riemann.cc>
2013-06-09 14:28:52 -0400

    Site updated at 2013-06-09 18:28:52 UTC

---
+++ javascripts/octopress.js
@@ -0,0 +1,267 @@
+function getNav() {
+  var mainNav = $('ul.main-navigation, ul[role=main-navigation]').before('<fieldset class="mobile-nav">')
+  var mobileNav = $('fieldset.mobile-nav').append('<select>');
+  mobileNav.find('select').append('<option value="">Navigate&hellip;</option>');
+  var addOption = function(i, option) {
+    mobileNav.find('select').append('<option value="' + this.href + '">&raquo; ' + $(this).text() + '</option>');
+  }
+  mainNav.find('a').each(addOption);
+  $('ul.subscription a').each(addOption);
+  mobileNav.find('select').bind('change', function(event) {
+    if (event.target.value) { window.location.href = event.target.value; }
+  });
+}
+
+function addSidebarToggler() {
+  if(!$('body').hasClass('sidebar-footer')) {
+    $('#content').append('<span class="toggle-sidebar"></span>');
+    $('.toggle-sidebar').bind('click', function(e) {
+      e.preventDefault();
+      if ($('body').hasClass('collapse-sidebar')) {
+        $('body').removeClass('collapse-sidebar');
+      } else {
+        $('body').addClass('collapse-sidebar');
+      }
+    });
+  }
+  var sections = $('aside.sidebar > section');
+  if (sections.length > 1) {
+    sections.each(function(index, section){
+      if ((sections.length >= 3) && index % 3 === 0) {
+        $(section).addClass("first");
+      }
+      var count = ((index +1) % 2) ? "odd" : "even";
+      $(section).addClass(count);
+    });
+  }
+  if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
+}
+
+function testFeatures() {
+  var features = ['maskImage'];
+  $(features).map(function(i, feature) {
+    if (Modernizr.testAllProps(feature)) {
+      $('html').addClass(feature);
+    } else {
+      $('html').addClass('no-'+feature);
+    }
+  });
+  if ("placeholder" in document.createElement("input")) {
+    $('html').addClass('placeholder');
+  } else {
+    $('html').addClass('no-placeholder');
+  }
+}
+
+function addCodeLineNumbers() {
+  if (navigator.appName === 'Microsoft Internet Explorer') { return; }
+  $('div.gist-highlight').each(function(code) {
+    var tableStart = '<table><tbody><tr><td class="gutter">',
+        lineNumbers = '<pre class="line-numbers">',
+        tableMiddle = '</pre></td><td class="code">',
+        tableEnd = '</td></tr></tbody></table>',
+        count = $('.line', code).length;
+    for (var i=1;i<=count; i++) {
+      lineNumbers += '<span class="line-number">'+i+'</span>\n';
+    }
+    var table = tableStart + lineNumbers + tableMiddle + '<pre>'+$('pre', code).html()+'</pre>' + tableEnd;
+    $(code).html(table);
+  });
+}
+
+function flashVideoFallback(){
+  var flashplayerlocation = "/assets/jwplayer/player.swf",
+      flashplayerskin = "/assets/jwplayer/glow/glow.xml";
+  $('video').each(function(i, video){
+    video = $(video);
+    if (!Modernizr.video.h264 && swfobject.getFlashPlayerVersion() || window.location.hash.indexOf("flash-test") !== -1){
+      video.children('source[src$=mp4]').first().map(i, function(source){
+        var src = $(source).attr('src'),
+            id = 'video_'+Math.round(1 + Math.random()*(100000)),
+            width = video.attr('width'),
+            height = parseInt(video.attr('height'), 10) + 30;
+            video.after('<div class="flash-video"><div><div id='+id+'>');
+        swfobject.embedSWF(flashplayerlocation, id, width, height + 30, "9.0.0",
+          { file : src, image : video.attr('poster'), skin : flashplayerskin } ,
+          { movie : src, wmode : "opaque", allowfullscreen : "true" }
+        );
+      });
+      video.remove();
+    }
+  });
+}
+
+function wrapFlashVideos() {
+  $('object').each(function(i, object) {
+    if( $(object).find('param[name=movie]').length ){
+      $(object).wrap('<div class="flash-video">')
+    }
+  });
+  $('iframe[src*=vimeo],iframe[src*=youtube]').wrap('<div class="flash-video">')
+}
+
+function renderDeliciousLinks(items) {
+  var output = "<ul>";
+  for (var i=0,l=items.length; i<l; i++) {
+    output += '<li><a href="' + items[i].u + '" title="Tags: ' + (items[i].t == "" ? "" : items[i].t.join(', ')) + '">' + items[i].d + '</a></li>';
+  }
+  output += "</ul>";
+  $('#delicious').html(output);
+}
+
+//////////// Start Twitter Hack
+
+function prettyDate(time) {
+  if (navigator.appName === 'Microsoft Internet Explorer') {
+    return "<span>&infin;</span>"; // because IE date parsing isn't fun.
+  }
+  var say = {
+    just_now:    " now",
+    minute_ago:  "1m",
+    minutes_ago: "m",
+    hour_ago:    "1h",
+    hours_ago:   "h",
+    yesterday:   "1d",
+    days_ago:    "d",
+    last_week:   "1w",
+    weeks_ago:   "w"
+  };
+
+  var current_date = new Date(),
+      current_date_time = current_date.getTime(),
+      current_date_full = current_date_time + (1 * 60000),
+      date = new Date(time),
+      diff = ((current_date_full - date.getTime()) / 1000),
+      day_diff = Math.floor(diff / 86400);
+
+  if (isNaN(day_diff) || day_diff < 0) { return "<span>&infin;</span>"; }
+
+  return day_diff === 0 && (
+    diff < 60 && say.just_now ||
+    diff < 120 && say.minute_ago ||
+    diff < 3600 && Math.floor(diff / 60) + say.minutes_ago ||
+    diff < 7200 && say.hour_ago ||
+    diff < 86400 && Math.floor(diff / 3600) + say.hours_ago) ||
+    day_diff === 1 && say.yesterday ||
+    day_diff < 7 && day_diff + say.days_ago ||
+    day_diff === 7 && say.last_week ||
+    day_diff > 7 && Math.ceil(day_diff / 7) + say.weeks_ago;
+} 
+
+
+function linkifyTweet(text, url) {
+  // Linkify urls, usernames, hashtags
+  text = text.replace(/(https?:\/\/)([\w\-:;?&=+.%#\/]+)/gi, '<a href="$1$2">$2</a>')
+    .replace(/(^|\W)@(\w+)/g, '$1<a href="http://twitter.com/$2">@$2</a>')
+    .replace(/(^|\W)#(\w+)/g, '$1<a href="http://search.twitter.com/search?q=%23$2">#$2</a>');
+
+  // Use twitter's api to replace t.co shortened urls with expanded ones.
+  for (var u in url) {
+    if(url[u].expanded_url != null){
+      var shortUrl = new RegExp(url[u].url, 'g');
+      text = text.replace(shortUrl, url[u].expanded_url);
+      var shortUrl = new RegExp(">"+(url[u].url.replace(/https?:\/\//, '')), 'g');
+      text = text.replace(shortUrl, ">"+url[u].display_url);
+    }
+  }
+  return text
+}
+
+function render(tweets, twitter_user) {
+  var timeline = document.getElementById('tweets'),
+      content = '';
+
+  for (var t in tweets) {
+    content += '<li>'+'<p>'+'<a href="http://twitter.com/'+twitter_user+'/status/'+tweets[t].id_str+'">'+prettyDate(tweets[t].created_at)+'</a>'+linkifyTweet(tweets[t].text.replace(/\n/g, '<br>'), tweets[t].entities.urls)+'</p>'+'</li>';
+  }
+  timeline.innerHTML = content;
+}
+
+function getFeed(target){
+  target = $(target);
+  if (target.length == 0) return;
+  var user = target.attr('data-user');
+  var count = parseInt(target.attr('data-count'), 10);
+  var replies = target.attr('data-replies') == 'true';
+  $.ajax({
+      url: "http://api.twitter.com/1/statuses/user_timeline/" + user + ".json?trim_user=true&count=" + (count + 20) + "&include_entities=1&exclude_replies=" + (replies ? "0" : "1") + "&callback=?"
+    , dataType: 'jsonp'
+    , error: function (err) { $('#tweets li.loading').addClass('error').text("Twitter's busted"); }
+    , success: function(data) { render(data.slice(0, count), user); }
+  });
+}
+
+///// Finish Twitter Hack
+
+$('document').ready(function() {
+  testFeatures();
+//   wrapFlashVideos();
+//   flashVideoFallback();
+  addCodeLineNumbers();
+  getNav();
+  addSidebarToggler();
+  
+  /* Thumbnail helper. Disable animations, hide close button, arrows and slide to next gallery item if clicked */
+  /* http://jsfiddle.net/bG4gR/ */
+  $(".fancybox,a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").attr('rel', 'gallery').fancybox({
+          closeBtn  : true,
+          arrows    : true,
+          nextClick : true,
+          helpers : {
+                  overlay : {
+                    locked : false
+                  },
+                  thumbs : {
+                          width  : 50,
+                          height : 50
+                  }
+          }
+          /*
+          ,
+          beforeLoad: function(){
+          $(".fancybox-overlay").css({"overflow-y":"hidden"});
+          },
+          afterClose: function(){
+          $("body").css({"overflow-y":"visible"});
+          }
+          */
+  });  
+  
+  getFeed('#tweets'); // execute Twitter Hack
+});
+
+// iOS scaling bug fix
+// Rewritten version
+// By @mathias, @cheeaun and @jdalton
+// Source url: https://gist.github.com/901295
+(function(doc) {
+  var addEvent = 'addEventListener',
+      type = 'gesturestart',
+      qsa = 'querySelectorAll',
+      scales = [1, 1],
+      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+  function fix() {
+    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+    doc.removeEventListener(type, fix, true);
+  }
+  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+    fix();
+    scales = [0.25, 1.6];
+    doc[addEvent](type, fix, true);
+  }
+}(document));
+
+/*!	SWFObject v2.2 modified by Brandon Mathis to contain only what is necessary to dynamically embed flash objects
+  * Uncompressed source in javascripts/libs/swfobject-dynamic.js
+  * <http://code.google.com/p/swfobject/>
+	released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+*/
+var swfobject=function(){function s(a,b,d){var q,k=n(d);if(g.wk&&g.wk<312)return q;if(k){if(typeof a.id==l)a.id=d;if(g.ie&&g.win){var e="",c;for(c in a)if(a[c]!=Object.prototype[c])c.toLowerCase()=="data"?b.movie=a[c]:c.toLowerCase()=="styleclass"?e+=' class="'+a[c]+'"':c.toLowerCase()!="classid"&&(e+=" "+c+'="'+a[c]+'"');c="";for(var f in b)b[f]!=Object.prototype[f]&&(c+='<param name="'+f+'" value="'+b[f]+'" />');k.outerHTML='<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"'+e+">"+c+
+"</object>";q=n(a.id)}else{f=i.createElement(o);f.setAttribute("type",m);for(var h in a)a[h]!=Object.prototype[h]&&(h.toLowerCase()=="styleclass"?f.setAttribute("class",a[h]):h.toLowerCase()!="classid"&&f.setAttribute(h,a[h]));for(e in b)b[e]!=Object.prototype[e]&&e.toLowerCase()!="movie"&&(a=f,c=e,h=b[e],d=i.createElement("param"),d.setAttribute("name",c),d.setAttribute("value",h),a.appendChild(d));k.parentNode.replaceChild(f,k);q=f}}return q}function n(a){var b=null;try{b=i.getElementById(a)}catch(d){}return b}
+function t(a){var b=g.pv,a=a.split(".");a[0]=parseInt(a[0],10);a[1]=parseInt(a[1],10)||0;a[2]=parseInt(a[2],10)||0;return b[0]>a[0]||b[0]==a[0]&&b[1]>a[1]||b[0]==a[0]&&b[1]==a[1]&&b[2]>=a[2]?!0:!1}function u(a){return/[\\\"<>\.;]/.exec(a)!=null&&typeof encodeURIComponent!=l?encodeURIComponent(a):a}var l="undefined",o="object",m="application/x-shockwave-flash",v=window,i=document,j=navigator,g=function(){var a=typeof i.getElementById!=l&&typeof i.getElementsByTagName!=l&&typeof i.createElement!=l,
+b=j.userAgent.toLowerCase(),d=j.platform.toLowerCase(),g=d?/win/.test(d):/win/.test(b),d=d?/mac/.test(d):/mac/.test(b),b=/webkit/.test(b)?parseFloat(b.replace(/^.*webkit\/(\d+(\.\d+)?).*$/,"$1")):!1,k=!+"\u000b1",e=[0,0,0],c=null;if(typeof j.plugins!=l&&typeof j.plugins["Shockwave Flash"]==o){if((c=j.plugins["Shockwave Flash"].description)&&!(typeof j.mimeTypes!=l&&j.mimeTypes[m]&&!j.mimeTypes[m].enabledPlugin))k=!1,c=c.replace(/^.*\s+(\S+\s+\S+$)/,"$1"),e[0]=parseInt(c.replace(/^(.*)\..*$/,"$1"),
+10),e[1]=parseInt(c.replace(/^.*\.(.*)\s.*$/,"$1"),10),e[2]=/[a-zA-Z]/.test(c)?parseInt(c.replace(/^.*[a-zA-Z]+(.*)$/,"$1"),10):0}else if(typeof v.ActiveXObject!=l)try{var f=new ActiveXObject("ShockwaveFlash.ShockwaveFlash");if(f&&(c=f.GetVariable("$version")))k=!0,c=c.split(" ")[1].split(","),e=[parseInt(c[0],10),parseInt(c[1],10),parseInt(c[2],10)]}catch(h){}return{w3:a,pv:e,wk:b,ie:k,win:g,mac:d}}();return{embedSWF:function(a,b,d,i,k,e,c,f,h){var j={success:!1,id:b};if(g.w3&&!(g.wk&&g.wk<312)&&
+a&&b&&d&&i&&k){d+="";i+="";var p={};if(f&&typeof f===o)for(var m in f)p[m]=f[m];p.data=a;p.width=d;p.height=i;a={};if(c&&typeof c===o)for(var n in c)a[n]=c[n];if(e&&typeof e===o)for(var r in e)typeof a.flashvars!=l?a.flashvars+="&"+r+"="+e[r]:a.flashvars=r+"="+e[r];if(t(k))b=s(p,a,b),j.success=!0,j.ref=b}h&&h(j)},ua:g,getFlashPlayerVersion:function(){return{major:g.pv[0],minor:g.pv[1],release:g.pv[2]}},hasFlashPlayerVersion:t,createSWF:function(a,b,d){if(g.w3)return s(a,b,d)},getQueryParamValue:function(a){var b=
+i.location.search||i.location.hash;if(b){/\?/.test(b)&&(b=b.split("?")[1]);if(a==null)return u(b);for(var b=b.split("&"),d=0;d<b.length;d++)if(b[d].substring(0,b[d].indexOf("="))==a)return u(b[d].substring(b[d].indexOf("=")+1))}return""}}}();
+
+
